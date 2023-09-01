3a651990728416a547d511d054ebdb68329d4f18
ldhandley <lindseyhandley@gmail.com>
2023-09-01 09:39:48 -0700

    adding and listing adventures endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 import os
 import json
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, sessionmaker
+from datetime import date, datetime
 
 app = Flask(__name__, static_folder='../frontend/hello-world-angular/dist/hello-world-angular')
 
@@ -14,10 +17,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+    # This sets up a one-to-many relationship between User and Adventure.
+    # When you query a user object, you can access its adventures with user.adventures attribute
+    adventures = relationship('Adventure', back_populates='user')
 
     def to_dict(self):
         return {
@@ -28,6 +38,22 @@ class User(db.Model):
 
     def __repr__(self):
         return self.username + " - " + self.email
+
+class Adventure(db.Model):
+    __tablename__ = 'adventures'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, default=date.today, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    mileage = db.Column(db.Float)
+    estimated_calories = db.Column(db.Float)
+
+    # Foreign key for the user
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    # This sets up a many-to-one relationship between Adventure and User.
+    # When you query an adventure object, you can access its user with adventure.user attribute
+    user = relationship('User', back_populates='adventures')
 
 @app.route('/add_user/<username>/<email>')
 def add_user(username, email):
@@ -43,6 +69,69 @@ def list_users():
     print(users)
     users_json = json.dumps([user.to_dict() for user in users])
     return users_json
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+SessionLocal = sessionmaker(bind=engine)
+
+@app.route("/add_adventure", methods=["POST"])
+def add_adventure():
+    session = SessionLocal()
+
+    try:
+        data = request.json
+
+        user_id = data['user_id']
+        user = session.query(User).filter(User.id == user_id).first()
+        
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+        
+        # Parse the date string into a date object
+        date_object = datetime.strptime(data['date'], '%Y-%m-%d').date()
+
+        adventure = Adventure(
+            date = date_object,
+            category = data['category'],
+            mileage = data['mileage'],
+            estimated_calories = data['estimated_calories'],
+            user = user
+        )
+
+        session.add(adventure)
+        session.commit()
+        return jsonify({"message": "Adventure added successfully", "adventure_id": adventure.id}), 201
+    except Exception as e:
+        session.rollback()
+        return jsonify({"error": str(e)}), 400
+    finally:
+        session.close()
+
+@app.route("/list_adventures/<int:user_id>", methods=["GET"])
+def list_adventures(user_id):
+    session = SessionLocal()
+
+    try:
+        user = session.query(User).filter(User.id == user_id).first()
+        
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+
+        adventures = session.query(Adventure).filter(Adventure.user_id == user_id).all()
+
+        # Convert the list of adventures to a list of dictionaries to be JSON serialized
+        adventures_list = [{
+            "id": adventure.id,
+            "date": adventure.date.strftime('%Y-%m-%d'),  # Format the date for output
+            "category": adventure.category,
+            "mileage": adventure.mileage,
+            "estimated_calories": adventure.estimated_calories
+        } for adventure in adventures]
+
+        return jsonify(adventures_list)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
